51c83950198ab9202a8b5b30ebca4c042558b31f
galo <juanba911@gmail.com>
2022-06-14 20:51:01 -0300

    eslint option deleted

--- src/steps/access_the_site.ts
+++ src/steps/access_the_site.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import { ICustomWorld } from '../support/custom-world';
 import { Given, When, Then } from '@cucumber/cucumber';
 import { expect } from '@playwright/test';
@@ -10,7 +9,7 @@ Given('That a user opens the browser', async function (this: ICustomWorld) {
 
 When('The user navigate to the site', async function (this: ICustomWorld) {
   const page = this.page!;
-  await page.goto('https://platzi.com/');
+  await page.goto('https://platzi.com');
 });
 
 Then('The user should see the landing page', async function (this: ICustomWorld) {
