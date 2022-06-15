64b6048b4002430086b98c79331867df85af16ab
galo <juanba911@gmail.com>
2022-06-15 20:46:26 -0300

    move the when step

--- src/steps/general_steps.ts
+++ src/steps/general_steps.ts
@@ -1,7 +1,32 @@
 import { ICustomWorld } from '../support/custom-world';
-import { Given } from '@cucumber/cucumber';
+import { Given, When } from '@cucumber/cucumber';
 
 Given('A user is in the landing page', async function (this: ICustomWorld) {
   const page = this.page!;
   await page.goto('https://platzi.com');
+});
+
+When('The user searchs a topic', async function (this: ICustomWorld) {
+  const page = this.page!;
+  const searchInput = page.locator('.NewSearch.NewSearch--noLogged > form > div > input');
+  const searchButton = page.locator('.NewSearch-button > svg');
+  await searchInput.fill('javascript');
+  await searchButton.click();
+  await page.waitForTimeout(3000);
+});
+
+Given('A user is in the platzi blog landing page', async function (this: ICustomWorld) {
+  const page = this.page!;
+  await page.goto('https://platzi.com');
+  await page.click('div.Menu-dropdown > div > svg');
+  await page.click('li:nth-child(2) > a > div');
+});
+
+When('The user searchs a blog topic', async function (this: ICustomWorld) {
+  const page = this.page!;
+  const searchInput = page.locator('div.col-xs-12.col-md-5 > form > input');
+  const searchButton = page.locator('div.col-xs-12.col-md-5 > form > div > input[type=submit]');
+  await searchInput.fill('javascript');
+  await searchButton.click();
+  await page.waitForTimeout(3000);
 });
--- src/steps/topic_search.ts
+++ src/steps/topic_search.ts
@@ -1,20 +1,12 @@
 import { ICustomWorld } from '../support/custom-world';
-import { When, Then } from '@cucumber/cucumber';
+import { Then } from '@cucumber/cucumber';
 import { expect } from '@playwright/test';
-
-When('The user searchs a topic', async function (this: ICustomWorld) {
-  const page = this.page!;
-  const searchInput = page.locator('.NewSearch.NewSearch--noLogged > form > div > input');
-  const searchButton = page.locator('.NewSearch-button > svg');
-  await searchInput.fill('typescript');
-  await searchButton.click();
-  await page.waitForTimeout(3000);
-});
 
 Then('A list of results should be displayed', async function (this: ICustomWorld) {
   const page = this.page!;
-  await page.waitForTimeout(3000);
   const resultsList = await page.locator('.CourseList > article').count();
+  // eslint-disable-next-line no-console
+  console.log(resultsList);
   try {
     expect(resultsList).toBeGreaterThan(0);
   } catch (error) {
