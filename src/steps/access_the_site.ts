f28f36d965cf3bbf333286b15f011818f782bb0c
galo <juanba911@gmail.com>
2022-06-16 01:05:49 -0300

    elements modularized

--- src/steps/access_the_site.ts
+++ src/steps/access_the_site.ts
@@ -1,18 +1,20 @@
 import { ICustomWorld } from '../support/custom-world';
+import { urlMap } from '../pom';
+import { selectors } from '../utils/element_selectors';
 import { Given, When, Then } from '@cucumber/cucumber';
 import { expect } from '@playwright/test';
 
 Given('That a user opens the browser', async function (this: ICustomWorld) {
   const page = this.page!;
-  await page.goto('https://google.com');
+  await page.goto(urlMap.get('default'));
 });
 
 When('The user navigate to the site', async function (this: ICustomWorld) {
   const page = this.page!;
-  await page.goto('https://platzi.com');
+  await page.goto(urlMap.get('platzi'));
 });
 
 Then('The user should see the landing page', async function (this: ICustomWorld) {
   const page = this.page!;
-  expect(await page.waitForSelector('.ButtonLogin', { state: 'visible' })).toBeTruthy;
+  expect(await page.waitForSelector(selectors.loginBtn, { state: 'visible' })).toBeTruthy;
 });
