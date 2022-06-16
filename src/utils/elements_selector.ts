b742f5061672f39124ed46013b0ea2f6b10aa0c3
galo <juanba911@gmail.com>
2022-06-16 01:43:17 -0300

    elements added

--- src/utils/elements_selector.ts
+++ src/utils/elements_selector.ts
@@ -1,4 +1,10 @@
 export const selector = {
-  loginBtn: '.ButtonLogin',
-  courseList: '.CourseList > article',
+  loginBtn: '.ButtonLogin', //Login button
+  searchInput: '.NewSearch.NewSearch--noLogged > form > div > input', //Landing page search box input
+  searchBtn: '.NewSearch-button > svg', //Landing page search box button
+  courseList: '.CourseList > article', //List of searched courses
+  menu: 'div.Menu-dropdown > div > svg', //Landing page Dropdown menu
+  blog: 'li:nth-child(2) > a > div', //Platzi blog
+  blogSearchInput: 'div.col-xs-12.col-md-5 > form > input', //Blog search box input
+  blogSearchBtn: 'div.col-xs-12.col-md-5 > form > div > input[type=submit]', //Blog search button
 };
