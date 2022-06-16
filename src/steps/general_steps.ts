7b39cdf5683bf97b39273a5445d84db0f272d2fe
galo <juanba911@gmail.com>
2022-06-16 01:43:46 -0300

    elements modularized

--- src/steps/general_steps.ts
+++ src/steps/general_steps.ts
@@ -1,32 +1,30 @@
 import { ICustomWorld } from '../support/custom-world';
+import { urlMap } from '../pom';
+import { selector } from '../utils/elements_selector';
 import { Given, When } from '@cucumber/cucumber';
 
 Given('A user is in the landing page', async function (this: ICustomWorld) {
   const page = this.page!;
-  await page.goto('https://platzi.com');
+  await page.goto(urlMap.get('platzi'));
 });
 
 When('The user searchs a topic', async function (this: ICustomWorld) {
   const page = this.page!;
-  const searchInput = page.locator('.NewSearch.NewSearch--noLogged > form > div > input');
-  const searchButton = page.locator('.NewSearch-button > svg');
-  await searchInput.fill('javascript');
-  await searchButton.click();
+  await page.fill(selector.searchInput, 'javascript');
+  await page.click(selector.searchBtn);
   await page.waitForTimeout(3000);
 });
 
 Given('A user is in the platzi blog landing page', async function (this: ICustomWorld) {
   const page = this.page!;
-  await page.goto('https://platzi.com');
-  await page.click('div.Menu-dropdown > div > svg');
-  await page.click('li:nth-child(2) > a > div');
+  await page.goto(urlMap.get('platzi'));
+  await page.click(selector.menu);
+  await page.click(selector.blog);
 });
 
 When('The user searchs a blog topic', async function (this: ICustomWorld) {
   const page = this.page!;
-  const searchInput = page.locator('div.col-xs-12.col-md-5 > form > input');
-  const searchButton = page.locator('div.col-xs-12.col-md-5 > form > div > input[type=submit]');
-  await searchInput.fill('javascript');
-  await searchButton.click();
+  await page.fill(selector.blogSearchInput, 'javascript');
+  await page.click(selector.blogSearchBtn);
   await page.waitForTimeout(3000);
 });
