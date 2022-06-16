7e890727e5dc3032d44916dc2041f9972dea291e
galo <juanba911@gmail.com>
2022-06-16 01:41:47 -0300

    elements modularized

--- src/steps/topic_search.ts
+++ src/steps/topic_search.ts
@@ -1,12 +1,11 @@
 import { ICustomWorld } from '../support/custom-world';
+import { selector } from '../utils/elements_selector';
 import { Then } from '@cucumber/cucumber';
 import { expect } from '@playwright/test';
 
 Then('A list of results should be displayed', async function (this: ICustomWorld) {
   const page = this.page!;
-  const resultsList = await page.locator('.CourseList > article').count();
-  // eslint-disable-next-line no-console
-  console.log(resultsList);
+  const resultsList = await page.locator(selector.courseList).count();
   try {
     expect(resultsList).toBeGreaterThan(0);
   } catch (error) {
